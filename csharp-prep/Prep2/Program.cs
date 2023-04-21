4ca8f0c591f72a973397d9e79e43f3c038b459bc
Caioedu93 <depaulacaio10@gmail.com>
2023-04-21 12:48:00 -0300

    Prep1 and Prep2 completed.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,67 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade?");
+        string number= Console.ReadLine();
+        int Grade = int.Parse(number);
+        
+        if (Grade >= 94)
+        {
+            Console.WriteLine("Your Grade is A.");
+        }
+        else if (Grade >= 90)
+        {
+            Console.WriteLine("Your Grade is A-.");
+        }
+        else if (Grade >= 87)
+        {
+            Console.WriteLine("Your Grade is B+.");
+        }
+        else if (Grade >= 84)
+        {
+            Console.WriteLine("Your Grade is B.");
+        }
+        else if (Grade >= 80)
+        {
+            Console.WriteLine("Your Grade is B-.");
+        }
+        else if (Grade >= 77)
+        {
+            Console.WriteLine("Your Grade is C+.");
+        }
+        else if (Grade >= 74)
+        {
+            Console.WriteLine("Your Grade is C.");
+        }
+        else if (Grade >= 70)
+        {
+            Console.WriteLine("Your Grade is C-.");
+        }
+        else if (Grade >= 67)
+        {
+            Console.WriteLine("Your Grade is D+.");
+        }
+        else if (Grade >= 64)
+        {
+            Console.WriteLine("Your Grade is D.");
+        }
+        else if (Grade >= 60)
+        {
+            Console.WriteLine("Your Grade is D-.");
+        }
+        else 
+        {
+            Console.WriteLine("Your Grade is F.");
+        }
+
+        if (Grade >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the the course.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the course, try Harder next time.");
+        }
     }
+
 }
