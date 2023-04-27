417161eec356c40ee534c1242429a8d46a262456
Caioedu93 <depaulacaio10@gmail.com>
2023-04-26 23:02:13 -0300

    prep 3, 4 and 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+
+        int usernumber = -1;
+        while (usernumber !=0)
+        {
+           Console.Write("Enter a number(0 to quit):");
+           
+           string userReasponse = Console.ReadLine();
+           usernumber = int.Parse(userReasponse);
+
+           if (usernumber != 0)
+           {
+            numbers.Add(usernumber);
+           }
+        } 
+
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Part 3: Find the max
+        // There are several ways to do this, such as sorting the list
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
