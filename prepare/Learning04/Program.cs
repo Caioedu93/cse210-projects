4c6c61ec4d2e7d24d8fe702b7b1564a76e4ddd8d
Caioedu93 <depaulacaio10@gmail.com>
2023-06-01 21:06:42 -0300

    week 7

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment name1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(name1.GetSummary());
+
+        
+        MathAssignment name2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(name2.GetSummary());
+        Console.WriteLine(name2.GetHomeworkList());
+
+        WritingAssignment name3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(name3.GetSummary());
+        Console.WriteLine(name3.GetWritingInformation());
     }
 }
